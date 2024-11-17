7cbf25ddaf453df87a6e7e68b337276293df3b67
Dwaha Daud <dwahadaud68@gmail.com>
2024-11-16 19:02:17 -0500

    intial commit

---
+++ src/lib.rs
@@ -0,0 +1,159 @@
+use labeled::buckle::{Buckle, Component};
+use serde::{Deserialize, Serialize};
+
+/*************************************************
+DENT OPEN
+*************************************************/
+
+#[derive(serde::Deserialize, serde::Serialize, Debug)]
+pub struct DentOpen {
+    pub fd: u64,
+    pub entry: Option<dent_open::Entry>
+}
+
+
+pub mod dent_open {
+    #[derive(serde::Deserialize, serde::Serialize, Debug)]
+        pub enum Entry {
+        Name(String),
+        Facet(super::Buckle)
+    }
+}
+
+/*************************************************
+DENT KIND
+*************************************************/
+
+pub enum DentKind {
+    DentDirectory = 0,
+    DentFile = 1,
+    DentFacetedDirectory = 2,
+    DentGate = 3,
+    DentService = 4,
+    DentBlob = 5
+}
+
+impl From<DentKind> for i32 {
+    fn from(item: DentKind) -> Self {
+        match item {
+            DentKind::DentDirectory => 0,
+            DentKind::DentFile => 1,
+            DentKind::DentFacetedDirectory => 2,
+            DentKind::DentGate => 3,
+            DentKind::DentService => 4, 
+            DentKind::DentBlob => 5
+        }
+    }
+}
+
+/*************************************************
+DENT CREATE
+*************************************************/
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DentCreate {
+    pub label: Option<Buckle>,
+    pub kind: Option<dent_create::Kind>
+}
+
+pub mod dent_create {
+    #[derive(super::Serialize, super::Deserialize, Debug)]
+    pub enum Kind {
+        Directory,
+        File,
+        FacetedDirectory,
+        Gate(super::Gate),
+        Service(super::Service),
+        Blob
+    }
+}
+
+/*************************************************
+DENT UPDATE
+*************************************************/
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DentUpdate {
+    pub fd: u64,
+    pub kind: Option<dent_update::Kind>
+}
+
+pub mod dent_update {
+    #[derive(super::Serialize, super::Deserialize, Debug)]
+    pub enum Kind {
+        File(Vec<u8>),
+        Gate(super::Gate),
+        Service(super::Service),
+        Blob(u64)
+    }
+}
+/*************************************************
+GATES
+*************************************************/
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Gate {
+    pub kind: Option<gate::Kind>
+}
+
+pub mod gate {
+    #[derive(super::Serialize, super::Deserialize, Debug)]
+    pub enum Kind {
+        Direct(super::DirectGate),
+        Redirect(super::RedirectGate)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DirectGate {
+    pub privilege: Option<Component>,
+    pub invoker_integrity_clearance: Option<Component>,
+    pub function: Option<Function>,
+    pub declassify: Option<Component>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RedirectGate {
+    pub privilege: Option<Component>,
+    pub invoker_integrity_clearance: Option<Component>,
+    pub gate: u64,
+    pub declassify: Option<Component>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Service {
+    pub privilege: Option<Component>,
+    pub invoker_integrity_clearance: Option<Component>,
+    pub taint: Option<Buckle>,
+    pub url: String,
+    pub verb: i32,
+    pub headers: std::collections::HashMap<String, String>
+}
+
+/*************************************************
+FUNCTION
+*************************************************/
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Function {
+    pub memory: u64,
+    pub app_image: u64,
+    pub runtime: u64,
+    pub kernel: u64
+}
+
+/*************************************************
+RESULTS
+*************************************************/
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DentResult {
+    pub success: bool,
+    pub fd: Option<u64>,
+    pub data: Option<Vec<u8>>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DentOpenResult {
+    pub success: bool,
+    pub fd: u64,
+    pub kind: i32
+}
